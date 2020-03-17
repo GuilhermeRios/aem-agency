eac9acb7426dc83272b05d672e566e81e65c7e9a
Guilherme Rios <guilherme.rios@possible.com>
2020-03-17 17:18:23 -0300

    Add theme resources

---
+++ ui.frontend/src/main/webpack/site/main.js
@@ -0,0 +1,9 @@
+import jquery from 'jquery';
+window.$ = window.jQuery = jquery;
+require("../base/js/jquery.easing.min.js");
+require("../base/js/tether.js");
+require("../base/js/bootstrap.js");
+require("../base/js/slick.js");
+require("../base/js/isotope.pkgd.min.js");
+require("../base/js/odometer.min.js");
+require("../base/js/main.js");
--- ui.frontend/src/main/webpack/site/main.ts
+++ ui.frontend/src/main/webpack/site/main.ts
@@ -2,5 +2,6 @@
 import "./main.scss";
 
 // Typescript/Javascript
+import "./main.js";
 import "../components/**/*.js";
 import "../components/**/*.ts";
